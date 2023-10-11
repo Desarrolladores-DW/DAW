400107d8c6c4ff04aa9a549714350eadb4af5bd3
Jovani1827 <144496402+Jovani1827@users.noreply.github.com>
2023-10-10 22:26:16 -0600

    Add files via upload

---
+++ DAW-main/DAW-main/Login_CodeLearn/php/registro_usuario_be.php
@@ -0,0 +1,60 @@
+<?php
+include 'conexion_be.php';
+
+$nombre_completo = $_POST['nombre_completo'];
+$correo = $_POST['correo'];
+$usuario = $_POST['usuario'];
+$contrasena = $_POST['contrasena'];
+
+//Encriptar contraseña
+$contrasena = hash('sha512', $contrasena);
+
+$query = "INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena) 
+          VALUES('$nombre_completo','$correo', '$usuario', '$contrasena') ";
+          
+//Verificacion de correo en BD
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo= '$correo' ");
+
+        if(mysqli_num_rows($verificar_correo) > 0){
+            echo '
+            <script>
+                alert(" Este correo ya esta registrado ");
+                window.location = "../index_login.php";
+            </script>
+            ';
+            exit();
+            mysqli_close($conexion);
+        }
+//Verificacion de usuario en BD
+    $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario= '$usuario' ");
+
+        if(mysqli_num_rows($verificar_usuario) > 0){
+            echo '
+                <script>
+                    alert(" Este usuario ya esta registrado ");
+                    window.location = "../index_login.php";
+                </script>
+            ';
+    exit();
+    mysqli_close($conexion);
+}  
+
+        $ejecutar = mysqli_query($conexion, $query);
+
+          if($ejecutar){
+            echo '
+                <script>
+                    alert(" Usuario almacenado correctamente ");
+                    window.location = "../index_login.php";
+            </script>
+            ';
+          }else{
+            echo '
+                <script>
+                    alert("Intentalo de nuevo");
+                    window.location = "../index_login.php";
+                </script>
+            ';
+          }
+          mysqli_close($conexion);
+?>
