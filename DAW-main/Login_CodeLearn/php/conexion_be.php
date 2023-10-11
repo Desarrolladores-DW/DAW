ea4dfecce1a355bf7e17a6d5028cfaf40eddde7d
Jovani1827 <144496402+Jovani1827@users.noreply.github.com>
2023-10-10 22:30:34 -0600

    Add files via upload

---
+++ DAW-main/Login_CodeLearn/php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+    $conexion = mysqli_connect("Localhost", "root", "", "login_codelearn");
+    
+    /*   
+    if($conexion){
+        echo 'Conectado Exitosamente a la BD';
+    }else{
+        echo 'No se a podido conectar a la BD';
+    }
+    */
+
+?>
